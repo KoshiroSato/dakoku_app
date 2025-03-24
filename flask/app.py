5bc4355424406e240439e0ea23e440b2c7d939f8
Koshiro Sato <83401285ksatou.mcnlng@gmail.com>
2025-03-24 14:09:09 +0900

    同じ日に同じボタンを2度押せなくした。

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,25 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from func import init_db, insert_timestamp
 
 app = Flask(__name__)
 
+PROCESS_STATUS = {
+    'start': None,
+    'end': None,
+    'break': None,
+    'restart': None
+    }
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    today = datetime.now().strftime('%Y-%m-%d')
     if request.method == 'POST':
         stamp_value = request.form.get('stamp_value')
-        insert_timestamp(stamp_value)
+        # 同じ日に2度同じボタンは押せない
+        if PROCESS_STATUS[stamp_value] != today:
+            insert_timestamp(stamp_value)
+            PROCESS_STATUS[stamp_value] = today
     return render_template('index.html')
 
 if __name__ == '__main__':
