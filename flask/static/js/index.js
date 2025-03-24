193c90a41bbc94ece25841e35252b30276464427
Koshiro Sato <83401285ksatou.mcnlng@gmail.com>
2025-03-24 15:08:16 +0900

    取り消しボタンを追加した

--- flask/static/js/index.js
+++ flask/static/js/index.js
@@ -1,8 +1,24 @@
-function alert_func() {
+function disableMe(button) {
+  button.disabled = true;
+}
+
+function reactivateCancelButton() {
+  const cancel_button = document.getElementById('buttonA');
+  if (cancel_button.disabled) {
+    cancel_button.disabled = false;
+  }
+}
+
+function alertFunc() {
   alert('打刻されました。');
 }
 
-document.getElementById('start_btn').addEventListener('click', alert_func);
-document.getElementById('end_btn').addEventListener('click', alert_func);
-document.getElementById('break_btn').addEventListener('click', alert_func);
-document.getElementById('restart_btn').addEventListener('click', alert_func);
+function cancelAlertFunc() {
+  alert('直前の打刻は取り消しされました。');
+}
+
+document.getElementById('start_btn').addEventListener('click', alertFunc);
+document.getElementById('end_btn').addEventListener('click', alertFunc);
+document.getElementById('break_btn').addEventListener('click', alertFunc);
+document.getElementById('restart_btn').addEventListener('click', alertFunc);
+document.getElementById('cancel_btn').addEventListener('click', cancelAlertFunc)
