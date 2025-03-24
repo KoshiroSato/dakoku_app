0a382097c32de2b2e656711c98c7b260492d438b
Koshiro Sato <83401285ksatou.mcnlng@gmail.com>
2025-03-24 15:53:27 +0900

    取り消しボタンの無効化を追加した

--- flask/static/js/index.js
+++ flask/static/js/index.js
@@ -1,24 +1,11 @@
-function disableMe(button) {
+function disableCancelButton(button) {
   button.disabled = true;
-}
-
-function reactivateCancelButton() {
-  const cancel_button = document.getElementById('buttonA');
-  if (cancel_button.disabled) {
-    cancel_button.disabled = false;
-  }
-}
-
-function alertFunc() {
-  alert('打刻されました。');
-}
-
-function cancelAlertFunc() {
   alert('直前の打刻は取り消しされました。');
 }
 
-document.getElementById('start_btn').addEventListener('click', alertFunc);
-document.getElementById('end_btn').addEventListener('click', alertFunc);
-document.getElementById('break_btn').addEventListener('click', alertFunc);
-document.getElementById('restart_btn').addEventListener('click', alertFunc);
-document.getElementById('cancel_btn').addEventListener('click', cancelAlertFunc)
+function enableCancelButton(event) {
+  event.preventDefault();
+  const cancel_button = document.getElementById('cancel_btn');
+  cancel_button.disabled = false;
+  alert('打刻されました。');
+}
